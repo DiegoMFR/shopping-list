0cc8a4bea314e7743860d2faebc30dbf742fbf74
Diego Morales <diego.moralesfrancomano@evbox.com>
2024-11-08 16:24:05 +0100

    paralelize

--- src/app/(queries)/products.ts
+++ src/app/(queries)/products.ts
@@ -22,38 +22,38 @@ export type ShoppingList = Array<ShoppingItem>;
 
 export async function getProductsForList(
   listId: string
-): Promise<Array<ShoppingItem | null>> {
-  const { rows }: { rows: Array<ListedProduct> } =
-    await sql`SELECT * FROM list_products WHERE list=${listId}`;
+): Promise<(ShoppingItem | null)[]> {
+  const { rows } = await sql`SELECT * FROM list_products WHERE list=${listId}`;
 
-  let response: unknown[] | PromiseLike<(ShoppingItem | null)[]> = [];
+  let response: (ShoppingItem | null)[] = [];
 
   if (rows.length !== 0) {
-    for(const row of rows) {
-      const res = await getProductById(row.product);
-      console.log('111', res);
-      response = [...response, res];
+    const products = await getProductsById(rows.map(row => row.product ?? null));
+    if (products !== null) {
+      response = products;
     }
   }
 
-  console.log("222", response);
-
-  return response as (ShoppingItem | null)[];
+  return response;
 }
 
-export async function getProductById(
-  productId: string
-): Promise<ShoppingItem | null> {
-  const { rows } =
-    await sql`SELECT * FROM Products WHERE id=${productId} FETCH FIRST ROW ONLY;`;
-  if (rows.length === 0) {
+
+export async function getProductsById(
+  products: Array<string | null>
+): Promise<ShoppingList | null> {
+  
+  const results = await Promise.all(products.map(product => sql`SELECT * FROM Products WHERE id=${product} FETCH FIRST ROW ONLY;`))
+  
+  console.log('333', results);
+
+  if (results.length === 0) {
     return null;
   }
-  // Map the first row to the ShoppingItem type
-  const product: ShoppingItem = {
-    id: rows[0].id,
-    name: rows[0].name,
-    icon: rows[0].icon,
-  };
-  return product;
+  return results.map( result => {
+    return {
+      id: result.rows[0].id,
+      name: result.rows[0].name,
+      icon: result.rows[0].icon,
+    };
+  })
 }
